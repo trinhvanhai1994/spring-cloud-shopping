fe46d133c0f060eebe4e25296707960aabeac3c8
HaiTv <haitv@ominext.com>
2020-05-29 18:47:24 +0700

    init commit

---
+++ shopping-ui/src/main/java/com/ominext/shopping/ShoppingUiApplication.java
@@ -0,0 +1,13 @@
+package com.ominext.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingUiApplication.class, args);
+	}
+
+}
